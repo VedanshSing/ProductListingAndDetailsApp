194dc5628de016100ab9afcaa63659229d1710b7
VedanshSingh <100754132+VedanshSing@users.noreply.github.com>
2026-06-18 18:15:12 +0530

    Remove duplicate Bootstrap CSS import

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import './index.css'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
